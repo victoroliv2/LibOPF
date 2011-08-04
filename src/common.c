e26d66c0cde3857d692d63846dcb48f5e2f4d998
Victor Oliveira <victormatheus@gmail.com>
2011-08-04 10:49:32 -0300

    changes in copyright

--- src/common.c
+++ src/common.c
@@ -1,21 +1,14 @@
 /*
-  Copyright (C) <2009> <Alexandre Xavier Falcão and João Paulo Papa>
+  Copyright (C) <2009-2011> <Alexandre Xavier Falcão and João Paulo Papa>
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	Redistributions of source code must retain the above copyright notice, this
+	list of conditions and the following disclaimer.
+	Redistributions in binary form must reproduce the above copyright notice,
+	this list of conditions and the following disclaimer in the documentation
+	and/or other materials provided with the distribution.
 
   please see full copyright in COPYING file.
+  
   -------------------------------------------------------------------------
   written by A.X. Falcão <afalcao@ic.unicamp.br> and by J.P. Papa
   <papa.joaopaulo@gmail.com>, Oct 20th 2008
