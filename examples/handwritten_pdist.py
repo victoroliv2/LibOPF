1f3a9216578e1239ad38288bc7d932b1e4326959
Victor Oliveira <victormatheus@gmail.com>
2011-09-26 19:13:37 -0300

    example of precomputed distances use

---
+++ examples/handwritten_pdist.py
@@ -0,0 +1,60 @@
+import time
+
+import numpy
+import libopf_py
+
+from scikits.learn import datasets, svm, metrics
+from scipy.spatial import distance
+
+digits = datasets.load_digits()
+
+# To apply an classifier on this data, we need to flatten the image, to
+# turn the data in a (samples, feature) matrix:
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+
+def run(split):
+  n_split = int(split*n_samples)
+
+  print ""
+  print "="*100
+  print ""
+
+  print "Split: %3.2f" % split
+  print "Size: %d, Classifying Size: %d, Testing Size: %d" % (n_samples, n_split, n_samples-n_split)
+
+  rand = numpy.random.permutation(n_samples)
+
+  random_data  = data[rand]
+  random_label = digits.target[rand]
+
+  dist = distance.squareform(distance.pdist(random_data, 'sqeuclidean'))
+  dist_train,  dist_test  = numpy.ascontiguousarray(dist [:n_split, :n_split]), numpy.ascontiguousarray(dist [:n_split, n_split:])
+
+  label_train, label_test = random_label[:n_split], random_label[n_split:]
+
+  print "-"*20, "OPF", "-"*20
+  def opf():
+
+    dist_train_32  = dist_train.astype (numpy.float32)
+    label_train_32 = label_train.astype(numpy.int32)
+    dist_test_32   = dist_test.astype  (numpy.float32)
+    label_test_32  = label_test.astype (numpy.int32)
+
+    O = libopf_py.OPF()
+
+    t = time.time()
+#    O.fit(dist_train_32, label_train_32, precomputed_distance=True)
+    O.fit(dist_train_32, label_train_32, precomputed_distance=True, learning="agglomerative", split=0.8)
+    print "OPF: time elapsed in fitting: %f secs" % (time.time()-t)
+
+    t = time.time()
+    predicted = O.predict(dist_test_32)
+    print "OPF: time elapsed in predicting: %f secs" % (time.time()-t)
+
+    print "Classification report for OPF:\n%s\n" % (metrics.classification_report(label_test_32, predicted))
+    print "Confusion matrix:\n%s" % metrics.confusion_matrix(label_test_32, predicted)
+
+  opf()
+
+run(0.8)
